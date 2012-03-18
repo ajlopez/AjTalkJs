87b54f930b6f55846988af85e7f66fa28aba73fc
ajlopez <webmaster@ajlopez.com>
2012-03-18 11:23:11 -0300

    lookup and sendMessage, initial implementation with test

--- tests/tests.js
+++ tests/tests.js
@@ -10,3 +10,19 @@ assert.notEqual(null, obj.$klass);
 assert.notEqual(null, obj.$variables);
 assert.equal(3, obj.$variables.length);
 
+var x = 0;
+
+var method = function() {
+	x = 1;
+};
+
+cls.defineMethod("setx", method);
+
+assert.notEqual(null, obj.lookup("setx"));
+assert.equal(method, obj.lookup("setx"));
+
+assert.equal(0, x);
+obj.sendMessage("setx", null);
+assert.equal(1, x);
+
+
