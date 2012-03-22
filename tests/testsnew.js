fb61b614db5b71190b2eaeffd802210ac59af9be
ajlopez <webmaster@ajlopez.com>
2012-03-22 09:27:43 -0300

    New Implementation, compileMethod: in class, sendmessage in class, with tests

--- tests/testsnew.js
+++ tests/testsnew.js
@@ -1,39 +1,54 @@
 
 var assert = require('assert');
 var ajtalk = require('../lib/ajtalknew.js');
+var Smalltalk = ajtalk.Smalltalk;
 
+// New Experimental Implementation tests
 
-// Experimental
+// native basicNew
+var obj = Smalltalk.Object.basicNew();
 
-var obj = ajtalk.Smalltalk.Object.basicNew();
+assert.equal(obj.klass, Smalltalk.Object);
 
-assert.equal(obj.klass, ajtalk.Smalltalk.Object);
+// compiled new
+var obj2 = Smalltalk.Object.new();
 
-var pointclass = ajtalk.Smalltalk.Object.defineSubclass('Point');
+assert.equal(obj2.klass, Smalltalk.Object);
+
+var pointclass = Smalltalk.Object.defineSubclass('Point');
 
 assert.ok(pointclass);
+assert.ok(Smalltalk.Point);
+assert.equal(pointclass, Smalltalk.Point);
+assert.ok(Smalltalk.Point.super);
+assert.equal(Smalltalk.Point.super, Smalltalk.Object);
 
 var point = pointclass.basicNew();
 
 assert.ok(point);
 assert.equal(point.klass, pointclass);
 
-assert.ok(ajtalk.Smalltalk.Point);
+var point2 = pointclass.new();
 
-ajtalk.Smalltalk.Object.defineMethod('add', function(x, y) { return x + y; });
+assert.ok(point2);
+assert.equal(point2.klass, pointclass);
+
+Smalltalk.Object.defineMethod('add', function(x, y) { return x + y; });
 
 assert.equal(3, obj.add(1, 2));
 assert.equal(5, point.add(3, 2));
 
 var compiler = new ajtalk.Compiler();
 
-var method = compiler.compileMethod("add: x to: y ^x+y", ajtalk.Smalltalk.Object);
+var method = compiler.compileMethod("add: x to: y ^x+y", Smalltalk.Object);
 assert.equal("add:to:", method.name);
-ajtalk.Smalltalk.Object.defineMethod(method.name, method);
+Smalltalk.Object.defineMethod(method.name, method);
 
 assert.equal(3, obj.add_to_(1, 2));
 assert.equal(5, point.add_to_(3, 2));
 
 assert.equal(3, obj.sendMessage("add_to_", [1, 2]));
 assert.equal(5, point.sendMessage("add_to_", [3, 2]));
+
+Smalltalk.Object.compileMethod_("add1: x ^ x + 1.");
 
