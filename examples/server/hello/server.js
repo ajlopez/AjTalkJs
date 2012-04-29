79ad1c8b5274f6936ad5ac88c11fae044e75df81
ajlopez <webmaster@ajlopez.com>
2012-04-29 17:33:48 -0300

    Node Hello example

---
+++ examples/server/hello/server.js
@@ -0,0 +1,23 @@
+
+var http = require('http');
+var ajtalk = require('../../../lib/ajtalk');
+ajtalk.load('HtmlCanvas.st');
+ajtalk.load('HtmlHelloPage.st');
+var Smalltalk = ajtalk.Smalltalk;
+
+console.log('AjTalk loaded');
+
+var page = Smalltalk.HtmlHelloPage.basicNew();
+
+function Response(res) {
+	this._write_ = function(text) { res.write(text); }
+}
+
+http.createServer(function(req,res) {
+	var html = Smalltalk.HtmlCanvas.basicNew();
+	html.setResponse_(res);
+	page.render_(html);
+	res.end();
+}).listen(8080);
+
+console.log('Server started at http://127.0.0.1:8080');
