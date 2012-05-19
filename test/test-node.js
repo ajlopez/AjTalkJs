a1174d137a25b737fcf2c6f295622577f9a6d163
ajlopez <webmaster@ajlopez.com>
2012-05-19 11:32:42 -0300

    test-node loading st files

--- test/test-node.js
+++ test/test-node.js
@@ -3,7 +3,7 @@ var ajtalk = require('../lib/ajtalk');
 var Smalltalk = ajtalk.Smalltalk;
 require('../lib/ajtalk-node.js');
 
-exports['Require Javascript'] = function(test) {
+exports['Require Javascript File'] = function(test) {
     test.ok(Smalltalk.Node);
     
     Smalltalk.Node.require_(__dirname + '/files/Point.st.js');
@@ -11,6 +11,16 @@ exports['Require Javascript'] = function(test) {
     test.ok(Smalltalk.Point);
     var point = Smalltalk.Point.basicNew();
     test.ok(point);
+	
+	test.done();
+};
+
+exports['Load st File'] = function(test) {
+    test.ok(Smalltalk.Node);
+    
+    Smalltalk.Node.loadst_(__dirname + '/files/HtmlHelloPage.st');
+
+    test.ok(Smalltalk.HtmlHelloPage);
 	
 	test.done();
 };
