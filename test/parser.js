474421a1141dad1d255377dc1f7176a0be829dae
ajlopez <webmaster@ajlopez.com>
2013-08-19 20:00:32 -0300

    Parser parse and compile integer, string

--- test/parser.js
+++ test/parser.js
@@ -13,3 +13,29 @@ exports['parse and compile integer'] = function (test) {
     test.ok(result);
     test.equal(result, '123');
 };
+
+exports['parse and compile integer with spaces'] = function (test) {
+    var myparser = parser.createParser('  123   ');    
+    
+    var expression = myparser.parse();
+    
+    test.ok(expression);
+    
+    var result = expression.compile();
+    
+    test.ok(result);
+    test.equal(result, '123');
+};
+
+exports['parse and compile string'] = function (test) {
+    var myparser = parser.createParser("'foo'");    
+    
+    var expression = myparser.parse();
+    
+    test.ok(expression);
+    
+    var result = expression.compile();
+    
+    test.ok(result);
+    test.equal(result, "'foo'");
+};
