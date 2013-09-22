b4dde7a698c7fc32f7ee44a06bd431ea7fc76281
ajlopez <webmaster@ajlopez.com>
2013-09-22 18:42:34 -0300

    Argument variables in context

--- test/context.js
+++ test/context.js
@@ -22,3 +22,14 @@ exports['context with local variable'] = function (test) {
     mycontext.defineLocalVariable('a');
     test.equal(mycontext.isLocalVariable('a'), true);
 }
+
+exports['context without argument'] = function (test) {
+    var mycontext = context.createContext();
+    test.equal(mycontext.isArgumentVariable('a'), false);
+}
+
+exports['context with argument'] = function (test) {
+    var mycontext = context.createContext();
+    mycontext.defineArgumentVariable('a');
+    test.equal(mycontext.isArgumentVariable('a'), true);
+}
