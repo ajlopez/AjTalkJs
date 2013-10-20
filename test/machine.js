2a168e54f84b1e50750dfe647497024da14191e3
ajlopez <webmaster@ajlopez.com>
2013-10-20 18:21:30 -0300

    Additional tests for machine block

--- test/machine.js
+++ test/machine.js
@@ -1,7 +1,64 @@
 
 var machine = require('../lib/machine');
+var ByteCodes = machine.ByteCodes;
 
 exports['machine bytecodes'] = function (test) {
     test.ok(machine.ByteCodes);
     test.equal(typeof machine.ByteCodes, 'object');
 }
+
+exports['create block'] = function (test) {
+    var result = machine.createBlock();
+    test.ok(result);
+}
+
+exports['add value'] = function (test) {
+    var block = machine.createBlock();
+    var result = block.addValue(1);
+    test.equal(result, 0);
+}
+
+exports['add two values'] = function (test) {
+    var block = machine.createBlock();
+    var result = block.addValue(1);
+    test.equal(result, 0);
+    var result = block.addValue(2);
+    test.equal(result, 1);
+}
+
+exports['add value twice'] = function (test) {
+    var block = machine.createBlock();
+    var result = block.addValue(1);
+    test.equal(result, 0);
+    var result = block.addValue(1);
+    test.equal(result, 0);
+}
+
+exports['return value'] = function (test) {
+    var block = machine.createBlock();
+    var position = block.addValue(1);
+    block.compileByteCode(ByteCodes.GetValue, position);
+    block.compileByteCode(ByteCodes.Return);
+    var result = block.toFunction().apply(null);
+    test.ok(result);
+    test.equal(result, 1);
+}
+
+exports['return first argument'] = function (test) {
+    var block = machine.createBlock();
+    block.compileByteCode(ByteCodes.GetArgument, 0);
+    block.compileByteCode(ByteCodes.Return);
+    var result = block.toFunction().apply(null, [10, 12]);
+    test.ok(result);
+    test.equal(result, 10);
+}
+
+exports['return second argument'] = function (test) {
+    var block = machine.createBlock();
+    block.compileByteCode(ByteCodes.GetArgument, 1);
+    block.compileByteCode(ByteCodes.Return);
+    var result = block.toFunction().apply(null, [10, 12]);
+    test.ok(result);
+    test.equal(result, 12);
+}
+
