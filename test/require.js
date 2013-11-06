9ea275eced936b80a120b944254cd92b97f3a679
ajlopez <webmaster@ajlopez.com>
2013-11-06 18:19:25 -0300

    Fixing require to load node modules from current directory; express applications modified run.js

--- test/require.js
+++ test/require.js
@@ -13,3 +13,16 @@ exports['require local module using Init.js'] = function (test) {
         process.chdir(cwd);
     }
 }
+
+exports['require local js module'] = function (test) {
+    var cwd = process.cwd();
+    try {
+        process.chdir(__dirname);
+        ajtalk.execute("Smalltalk require: 'answer'");
+        var result = ajtalk.Smalltalk.Answer;
+        test.equal(result, 42);
+    }
+    finally {
+        process.chdir(cwd);
+    }
+}
