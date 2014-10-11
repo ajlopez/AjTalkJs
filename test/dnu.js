68f1fb65f00c297789e2580f3a7d6585cd398b6d
ajlopez <webmaster@ajlopez.com>
2014-10-11 18:24:50 -0300

    DoesNotUnderstand with perform with

--- test/dnu.js
+++ test/dnu.js
@@ -9,3 +9,14 @@ exports['Invoke unknow selector'] = function (test) {
     
     test.equal(ajtalk.execute('SimpleDnu new foo'), 42);
 }
+
+exports['Use Proxy'] = function (test) {
+    var filename = path.join(__dirname, 'files', 'SimpleClass.st');
+    ajtalk.load(filename);
+    var filename = path.join(__dirname, 'files', 'SimpleProxy.st');
+    ajtalk.load(filename);
+
+    test.equal(ajtalk.execute('(SimpleProxy new: SimpleClass new) one'), 1);
+    test.equal(ajtalk.execute('(SimpleProxy new: SimpleSubclass new) one'), 1);
+    test.equal(ajtalk.execute('(SimpleProxy new: SimpleSubclass new) two'), 2);
+}
